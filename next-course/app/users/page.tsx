762593977c679c131ad449ff28ee0cdfe7a9d774
RYham <rafyahamza99@gmail.com>
2025-03-25 00:55:00 +0000

    Fetch data from the server

--- next-course/app/users/page.tsx
+++ next-course/app/users/page.tsx
@@ -1,7 +1,20 @@
-const UsersPage = () => {
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+
   return (
     <>
       <h1>User page</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </>
   );
 };
