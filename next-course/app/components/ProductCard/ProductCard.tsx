ee857bf81e5af0029e53d86d4a8c3f755afe398f
RYham <rafyahamza99@gmail.com>
2025-03-26 00:59:23 +0000

    Using module css style with next.js

--- next-course/app/components/ProductCard/ProductCard.tsx
+++ next-course/app/components/ProductCard/ProductCard.tsx
@@ -1,12 +1,14 @@
-import AddToCart from "./AddToCart";
+import AddToCart from "../AddToCart";
+import style from "./ProductCard.module.css";
 
 const ProductCard = () => {
+  console.log(style);
   return (
-    <>
+    <div className={style.card}>
       <p>Title of the card</p>
       <p>Descripton to the card</p>
       <AddToCart />
-    </>
+    </div>
   );
 };
 
