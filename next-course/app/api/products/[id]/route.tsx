97351695a9787113d8dad9124cb4e3b30c71fd89
RYham <rafyahamza99@gmail.com>
2025-04-07 20:23:13 +0100

    Exercise for creation a model in databse for products

--- next-course/app/api/products/[id]/route.tsx
+++ next-course/app/api/products/[id]/route.tsx
@@ -1,18 +1,29 @@
+import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
-export function GET(
+export async function GET(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
-  if (params.id > 11)
+  const productId: number = parseInt(params.id);
+
+  const product = await prisma.product.findUnique({
+    where: {
+      id: productId,
+    },
+  });
+
+  if (!product)
     return NextResponse.json(
       { error: "This product doesn't exist" },
       { status: 404 }
     );
 
-  return NextResponse.json({
-    id: params.id,
-    name: `Product ${params.id}`,
-    price: params.id * 2,
+  const newProduct = await prisma.product.findUnique({
+    where: {
+      id: productId,
+    },
   });
+
+  return NextResponse.json(newProduct);
 }
--- next-course/app/api/products/route.tsx
+++ next-course/app/api/products/route.tsx
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client";
 
 interface Product {
   id: number;
@@ -7,11 +8,8 @@ interface Product {
   price: number;
 }
 
-export function GET(request: NextRequest) {
-  const products: Product[] = [
-    { id: 1, name: "Milk", price: 2.5 },
-    { id: 2, name: "Bread", price: 3.5 },
-  ];
+export async function GET(request: NextRequest) {
+  const products: Product[] = await prisma.product.findMany();
 
   return NextResponse.json(products);
 }
@@ -24,8 +22,12 @@ export async function POST(request: NextRequest) {
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 404 });
 
-  return NextResponse.json(
-    { id: 10, name: body.name, price: body.price },
-    { status: 201 }
-  );
+  const newProduct = await prisma.product.create({
+    data: {
+      name: body.name,
+      price: body.price,
+    },
+  });
+
+  return NextResponse.json(newProduct, { status: 201 });
 }
