f64d6495ebefb14357bf13570080d430d531f732
RYham <rafyahamza99@gmail.com>
2025-04-11 23:26:32 +0100

    Showing Uploaded images

--- next-course/app/upload/page.tsx
+++ next-course/app/upload/page.tsx
@@ -1,15 +1,43 @@
 "use client";
-import { CldUploadWidget } from "next-cloudinary";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+import { useState } from "react";
+
+interface CldResult {
+  public_id: string;
+}
 
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+
   return (
-    <CldUploadWidget uploadPreset="dzvdkhtbz">
-      {({ open }) => (
-        <button className="btn btn-primary" onClick={() => open()}>
-          Upload
-        </button>
+    <>
+      {publicId && (
+        <CldImage
+          src={publicId}
+          width={270}
+          height={180}
+          alt="Uploaded image"
+        />
       )}
-    </CldUploadWidget>
+      <CldUploadWidget
+        uploadPreset="dzvdkhtbz"
+        onSuccess={(result, widget) => {
+          console.log(result);
+
+          if (result.event !== "success") return;
+
+          const info = result.info as CldResult;
+
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
   );
 };
 
