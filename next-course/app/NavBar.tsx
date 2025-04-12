52cf9c2d95f5e9b359374f3dac069666a0ee37d5
RYham <rafyahamza99@gmail.com>
2025-04-12 13:51:28 +0100

    Configuring Google Provider

--- next-course/app/NavBar.tsx
+++ next-course/app/NavBar.tsx
@@ -3,11 +3,12 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="flex p-2 bg-gray-600">
+    <div className="flex gap-2 p-2 bg-gray-600">
       <Link href="/" className="mr-5">
         Next.js
       </Link>
       <Link href="/users">Users</Link>
+      <Link href="/api/auth/signin">Login</Link>
     </div>
   );
 };
