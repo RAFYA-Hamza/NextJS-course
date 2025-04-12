a06f24ae099b84984d30eef55ee190aa8d113326
RYham <rafyahamza99@gmail.com>
2025-04-12 14:43:30 +0100

    Sign out users session

--- next-course/app/NavBar.tsx
+++ next-course/app/NavBar.tsx
@@ -12,7 +12,14 @@ const NavBar = () => {
       </Link>
       <Link href="/users">Users</Link>
       {status === "loading" && <div>Loading...</div>}
-      {status === "authenticated" && <div>{session.user?.name}</div>}
+      {status === "authenticated" && (
+        <div>
+          {session.user?.name}
+          <Link href="/api/auth/signout" className="ml-2 text-blue-300">
+            Sign out
+          </Link>
+        </div>
+      )}
 
       {status === "unauthenticated" && (
         <Link href="/api/auth/signin">Login</Link>
