e0703a392cfc4caa8d85c6d29e5651dd8378751f
RYham <rafyahamza99@gmail.com>
2025-04-16 12:11:12 +0100

    Optimizing Images

--- next-course/app/page.tsx
+++ next-course/app/page.tsx
@@ -2,14 +2,33 @@ import Link from "next/link";
 import ProductCard from "./components/ProductCard/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import Image from "next/image";
 
-export default async function Home() {
-  const session = await getServerSession(authOptions);
+import myImg from "../public/images/img.png";
+
+// export default async function Home() {
+//   const session = await getServerSession(authOptions);
+//   return (
+//     <main>
+//       <h1>Home {session && <span>{session.user?.name}</span>}</h1>
+//       <Link href="/users">Users</Link>
+//       <ProductCard />
+//     </main>
+//   );
+// }
+
+export default function page() {
   return (
-    <main>
-      <h1>Home {session && <span>{session.user?.name}</span>}</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard />
+    <main className="relative h-screen">
+      <Image
+        src="https://bit.ly/react-cover"
+        alt="My image"
+        fill
+        className="object-cover"
+        sizes="(max-width: 468px) 100vw, (max-width: 768px) 50vw, 33vw"
+        quality={100}
+        priority
+      />
     </main>
   );
 }
