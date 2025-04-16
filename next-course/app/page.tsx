267a81833c46a431acc5aee739d2af6d644cb4f4
RYham <rafyahamza99@gmail.com>
2025-04-16 23:23:07 +0100

    Using a custom fonts

--- next-course/app/page.tsx
+++ next-course/app/page.tsx
@@ -2,33 +2,33 @@ import Link from "next/link";
 import ProductCard from "./components/ProductCard/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
-import Image from "next/image";
+// import Image from "next/image";
 
-import myImg from "../public/images/img.png";
+// import myImg from "../public/images/img.png";
 
-// export default async function Home() {
-//   const session = await getServerSession(authOptions);
-//   return (
-//     <main>
-//       <h1>Home {session && <span>{session.user?.name}</span>}</h1>
-//       <Link href="/users">Users</Link>
-//       <ProductCard />
-//     </main>
-//   );
-// }
-
-export default function page() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
   return (
-    <main className="relative h-screen">
-      <Image
-        src="https://bit.ly/react-cover"
-        alt="My image"
-        fill
-        className="object-cover"
-        sizes="(max-width: 468px) 100vw, (max-width: 768px) 50vw, 33vw"
-        quality={100}
-        priority
-      />
+    <main>
+      <h1>Home {session && <span>{session.user?.name}</span>}</h1>
+      <Link href="/users">Users</Link>
+      <ProductCard />
     </main>
   );
 }
+
+// export default function page() {
+//   return (
+//     <main className="relative h-screen">
+//       <Image
+//         src="https://bit.ly/react-cover"
+//         alt="My image"
+//         fill
+//         className="object-cover"
+//         sizes="(max-width: 468px) 100vw, (max-width: 768px) 50vw, 33vw"
+//         quality={100}
+//         priority
+//       />
+//     </main>
+//   );
+// }
